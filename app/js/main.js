90c4727d3c5a377c0f4c8609f19fa93567067896
mejiagarcia <luiscarlos083@hotmail.com>
2017-02-19 21:25:04 -0500

    Agregé método de olvidar

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,44 @@
-var memoria={
-	"palabra": "",
-	"respuestas": []
+var memoria = {
+    "palabra": "",
+    "respuestas": []
 }
-memorias=[]
-// Metodo para obtener la palabra del usuario final
-function Decir() { 
-	return prompt("Dile algo a GIER")
+memorias = []
+    // Metodo para obtener la palabra del usuario final
+function Decir() {
+    return prompt("Dile algo a GIER")
 }
+
 function Escuchar() {
-	var palabra = Decir();
-	obtenerMemorias();
-	var respondio = false;
-	for (i in memorias) {
-		if(palabra==memorias[i].palabra){
-			Responder(palabra);
-			respondio=true;
-		}
-	}
-	if(respondio==false){
-		Aprender(palabra);
-	}
+    var palabra = Decir();
+    obtenerMemorias();
+    var respondio = false;
+    for (i in memorias) {
+        if (palabra == memorias[i].palabra) {
+            Responder(palabra);
+            respondio = true;
+        }
+    }
+    if (respondio == false) {
+        Aprender(palabra);
+    }
 }
-function Responder(palabra){
-	for (i in memorias) {
-		if(palabra==memorias[i].palabra){
-			alert(memorias[i].respuestas);
-			efectoAimst(palabra)
-		}
-	}
+
+function Responder(palabra) {
+    for (i in memorias) {
+        if (palabra == memorias[i].palabra) {
+            alert(memorias[i].respuestas);
+            efectoAimst(palabra)
+        }
+    }
 }
-function Aprender(palabra){
-	memoria={"palabra": palabra, "respuestas": prompt("¿Que debo decir?")}
-	memorias.push(memoria);
-	guardarMemorias();
-	alert("Palabra aprendida.");
+
+function Olvidar(palbra) {
+    return "olvidé todo.";
+}
+
+function Aprender(palabra) {
+    memoria = { "palabra": palabra, "respuestas": prompt("¿Que debo decir?") }
+    memorias.push(memoria);
+    guardarMemorias();
+    alert("Palabra aprendida.");
 }
